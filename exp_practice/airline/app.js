d64cb9dd9c064c210722acb800a4dd01b2ba1180
nick bouldien <nick.bouldien@gmail.com>
2017-04-25 22:15:15 -0700

    commit post testing with mocha, supertest, should example

--- exp_practice/airline/app.js
+++ exp_practice/airline/app.js
@@ -3,41 +3,43 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-//var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
 
-var app = express();
+module.exports = function (flights) {
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(function (req, res, next) {
-  res.set('X-Powered-By', 'Flight Tracker');
-  next();
-});
+  var express = require('express');
+  var routes = require('./routes')(flights) ;
+  //var user = require('./routes/user');
+  var path = require('path');
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  var app = express();
+
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function (req, res, next) {
+    res.set('X-Powered-By', 'Flight Tracker');
+    next();
+  });
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  app.get('/flight/:number', routes.flight);
+  app.put('/flight/:number/arrived', routes.arrived);
+  //app.get('/flight2', routes.flight2);
+  app.get('/list', routes.list);
+  //app.get('/users', user.list);
+
+  return app;
 }
-
-app.get('/flight/:number', routes.flight);
-app.put('/flight/:number/arrived', routes.arrived);
-//app.get('/flight2', routes.flight2);
-app.get('/list', routes.list);
-//app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
--- exp_practice/airline/routes/index.js
+++ exp_practice/airline/routes/index.js
@@ -2,56 +2,62 @@
 /*
  * GET home page.
  */
-let flights = require('../data');
- let flight = require('../flight');
 
-//  let flight1 = flight({
-// 	number: 1,
-// 	origin: 'LAX',
-// 	destination: 'DCA',
-//   departs: '9AM',
-//   arrives: '4PM'
-// });
-//
-// let flight2 = flight({
-// 	number: 2,
-// 	origin: 'LAX',
-// 	destination: 'PDX',
-//   departs: '10AM',
-//   arrives: '12PM'
-// });
+ module.exports = function(flights) {
+
+    let flight = require('../flight');
+
+   //  let flight1 = flight({
+   // 	number: 1,
+   // 	origin: 'LAX',
+   // 	destination: 'DCA',
+   //   departs: '9AM',
+   //   arrives: '4PM'
+   // });
+   //
+   // let flight2 = flight({
+   // 	number: 2,
+   // 	origin: 'LAX',
+   // 	destination: 'PDX',
+   //   departs: '10AM',
+   //   arrives: '12PM'
+   // });
 
 
-for(let number in flights){
-    flights[number] = flight(flights[number]);
-};
+   for(let number in flights){
+       flights[number] = flight(flights[number]);
+   };
 
-exports.flight = function(req, res){
-  let number = req.param('number');
-  if (typeof flights[number] === 'undefined') {
-    res.status(404).json({status: 'error'});
-  } else {
-    res.json(flights[number].getInformation());
-  }
-};
+   var functions = {};
 
-exports.arrived = function(req, res){
-  let number = req.param('number');
-  if (typeof flights[number] === 'undefined') {
-    res.status(404).json({status: 'error'});
-  } else {
-    flights[number].triggerArrive();
-    res.json({status: 'arrived!'});
-  }
-};
+   functions.flight = function(req, res){
+     let number = req.param('number');
+     if (typeof flights[number] === 'undefined') {
+       res.status(404).json({status: 'error'});
+     } else {
+       res.json(flights[number].getInformation());
+     }
+   };
 
-exports.list = function(req, res){
-  res.render('list', {title: 'All Flights', flights: flights});
-};
-// exports.flight1 = function(req, res){
-//   res.json(flight1.getInformation());
-// };
-//
-// exports.flight2 = function(req, res){
-//   res.json(flight2.getInformation());
-// };
+   functions.arrived = function(req, res){
+     let number = req.param('number');
+     if (typeof flights[number] === 'undefined') {
+       res.status(404).json({status: 'error'});
+     } else {
+       flights[number].triggerArrive();
+       res.json({status: 'arrived!'});
+     }
+   };
+
+   functions.list = function(req, res){
+     res.render('list', {title: 'All Flights', flights: flights});
+   };
+   // exports.flight1 = function(req, res){
+   //   res.json(flight1.getInformation());
+   // };
+   //
+   // exports.flight2 = function(req, res){
+   //   res.json(flight2.getInformation());
+   // };
+   return functions;
+ }
